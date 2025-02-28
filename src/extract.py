93dcc000c8a59c5ed3113bf0042a9e8ebea6c51d
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-02-28 05:23:45 -0300

    successfull connection to API

---
+++ src/extract.py
@@ -0,0 +1,9 @@
+import requests
+from config import API_KEY
+
+LAT = -32.8894155
+LON = -68.8446177
+URL = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}"
+
+response = requests.get(URL)
+print(response.json())
