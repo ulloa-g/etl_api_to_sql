9b0885dd599a7c5abc9901feb841f6c349235718
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-02-28 05:36:11 -0300

    update engineering units from API response

--- src/extract.py
+++ src/extract.py
@@ -3,7 +3,7 @@ from config import API_KEY
 
 LAT = -32.8894155
 LON = -68.8446177
-URL = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}"
+URL = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}&units=metric"
 
 response = requests.get(URL)
 print(response.json())
