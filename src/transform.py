e7fa7e112d81736e9c07d2dafed582d19f25c3a9
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-03-03 18:51:46 -0300

    update transform.py file

---
+++ src/transform.py
@@ -0,0 +1,9 @@
+from extract import df
+
+df['fecha'] = df['fecha_hora'].dt.strftime('%d/%m/%Y')
+df['hora'] = df['fecha_hora'].dt.strftime('%H:%M')
+df = df.drop(columns=['fecha_hora'])
+df = df[['fecha', 'hora', 'pais', 'ciudad', 'temp_actual',
+         'sensacion_termica', 'clima', 'descripcion_clima',
+         'temp_min', 'temp_max', 'velocidad_viento[m/s]']]
+print(df.head())
