35e2c5dd5db621f1bc94e77c643b4203a1c30a0c
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-03-03 19:57:34 -0300

    improve modularity on extract.py

--- src/extract.py
+++ src/extract.py
@@ -6,18 +6,24 @@ LAT = -32.8894155
 LON = -68.8446177
 URL = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}&units=metric&lang=es"
 
-response = requests.get(URL)
-data = response.json()
-data_dict = {
-    "fecha_hora": pd.to_datetime(data["dt"], unit="s"),
-    "pais": data["sys"]["country"],
-    "ciudad": data["name"],
-    "temp_actual": data["main"]["temp"],
-    "sensacion_termica": data["main"]["feels_like"],
-    "clima": data["weather"][0]["main"],
-    "descripcion_clima": data["weather"][0]["description"],
-    "temp_min": data["main"]["temp_min"],
-    "temp_max": data["main"]["temp_max"],
-    "velocidad_viento[m/s]": data["wind"]["speed"],
-}
-df = pd.DataFrame(data_dict, index=[0])
+
+def extract_data():
+    response = requests.get(URL)
+    data = response.json()
+    data_dict = {
+        "fecha_hora": pd.to_datetime(data["dt"], unit="s"),
+        "pais": data["sys"]["country"],
+        "ciudad": data["name"],
+        "temp_actual": data["main"]["temp"],
+        "sensacion_termica": data["main"]["feels_like"],
+        "clima": data["weather"][0]["main"],
+        "descripcion_clima": data["weather"][0]["description"],
+        "temp_min": data["main"]["temp_min"],
+        "temp_max": data["main"]["temp_max"],
+        "velocidad_viento[m/s]": data["wind"]["speed"],
+    }
+    df = pd.DataFrame(data_dict, index=[0])
+    return df
+
+
+df = extract_data()
