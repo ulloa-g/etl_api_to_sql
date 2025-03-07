45099a7c30fd706181597b9108c78cb29afc3f3e
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-03-06 23:34:02 -0300

    update to absolute path

--- run_etl.sh
+++ run_etl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source venv/bin/activate
+source /home/gabriel/repos/etl_api_to_sql/venv/bin/activate
 echo "Running ETL process"
-python3 run_etl.py
+/usr/bin/python3 /home/gabriel/repos/etl_api_to_sql/run_etl.py
 echo "ETL process completed"
 deactivate
